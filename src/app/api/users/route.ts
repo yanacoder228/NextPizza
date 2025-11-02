5337954236536b6a1037d70ef1e042224c5cc2d5
yanaesher <senyukyana26@gmail.com>
2025-11-02 22:31:52 +0100

    prisma post user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,22 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const users = await prisma.user.findMany();
   return NextResponse.json(users);
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { fullName, email, password } = body;
+
+  const user = await prisma.user.create({
+    data: {
+      fullName,
+      email,
+      password,
+    },
+  });
+
+  return NextResponse.json(user);
 }
