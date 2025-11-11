b8254a2e91c3b2833960fdbc00bbe2314c2f4c16
yanaesher <senyukyana26@gmail.com>
2025-11-11 14:00:25 +0100

    feat: api GET products & ingredients

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,8 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const products = await prisma.product.findMany();
+
+  return NextResponse.json(products);
+}
